1fe53e889282b94a91780af8a58f28a6e5831d39
Pedro Jiménez Díaz <fenomenog4@gmail.com>
2011-12-14 17:38:57 +0100

    Running shopify_app generator in a pristine rails 3.1 app.
    First steps toward success, so far so good

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShopifyOpenerpConnector::Application.config.secret_token = 'd1fa1cb8ed3dad0dbf2df203e649cb86a27f3efb07a18b691a8cda79f50e4dec6d4c3089d47ad01c4dafa4965e0e557ce2b4e1e0c38368d9a8fc226fffc509fd'
